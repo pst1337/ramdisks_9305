35c8295653df5262e632fdf64b0a8af26a1e915e
pst1337 <pst1337@gmail.com>
2014-04-17 17:50:11 +0200

    Add kernel features function

--- res/bc/bccontroller.sh
+++ res/bc/bccontroller.sh
@@ -11,6 +11,10 @@
 # kernel specification (hardware; type; target; url)
 KERNEL_SPECS="i9305;samsung;jb43;-"
 
+# kernel features 
+# (1=enable-busybox)
+KERNEL_FEATURES="-1-"
+
 # path to kernel libraries
 LIBPATH="/lib/modules"				# Samsung
 #LIBPATH="/system/lib/modules"		# Cyanogenmod+Omni
@@ -422,6 +426,11 @@ fi
 
 if [ "get_kernel_specs" == "$1" ]; then
 	echo $KERNEL_SPECS
+	exit 0
+fi
+
+if [ "get_kernel_features" == "$1" ]; then
+	echo $KERNEL_FEATURES
 	exit 0
 fi
 
